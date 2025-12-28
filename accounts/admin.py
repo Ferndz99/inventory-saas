661e1e3846504a14aace0b94f6d7c2d2755b2367
Sebastian <fernandezbarrera.sebastian@gmail.com>
2025-12-28 18:09:20 -0300

    feat(account-app): add account model to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ("email", "is_staff", "is_active", "created_at")
+    search_fields = ("email",)
+    list_filter = ("is_staff", "is_active")
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+
+admin.site.register(Account, AccountAdmin)
